58288a57cf17699a0ca5ef0cd1d6b92bae3fb980
stay18 <146612234+stay18@users.noreply.github.com>
2023-10-04 21:47:57 -0600

    Update Datos.php

--- Datos.php
+++ Datos.php
@@ -1,41 +1,6 @@
-<?php
-
-$nombre =$_POST["nombre"];
-$pass = $_POST["pass"];
-// Datos de conexión a la base de datos
-$servername = "localhost";
-$username = "adri";
-$password = "12345";
-$database = "lincoln";
-
-// Crear una conexión a la base de datos
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}else{
-    echo "la conexxion exxitosa";
-}
 
 
-
-if ($conn->query($sql) === TRUE) {
-    echo "Registro insertado correctamente";
-} else {
-    echo "Error al insertar registro: " . $conn->error;
-}
-
-
-
-
-// Ahora puedes ejecutar consultas SQL aquí
-
-// Cerrar la conexión cuando hayas terminado
-$conn->close();
-
-
-
+-------------------------------------------------------------------
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"];
@@ -47,14 +12,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fechaNacimiento = new DateTime($fechaNacimiento);
     $hoy = new DateTime();
     $edad = $hoy->diff($fechaNacimiento)->y;
-    
+
     // Puedes imprimir la edad para verificar
     // echo "Edad: " . $edad;
     
     // Aquí puedes realizar operaciones con los datos, como guardarlos en una base de datos
     // Por ejemplo, puedes usar una conexión a una base de datos MySQL para guardar los datos
     // Reemplaza los siguientes valores con los datos de tu base de datos
-  
+       $servername = "localhost";
+        $username = "adri";
+        $password = "12345";
+        $database = "lincoln";
+
+    
+    // Establecer conexión a la base de datos
+    $conexion = new mysqli($servidor, $usuario, $contrasena, $nombreBaseDatos);
+    
+    // Verificar la conexión
+    if ($conexion->connect_error) {
+        die("Error en la conexión a la base de datos: " . $conexion->connect_error);
+    }
     
     // Insertar los datos en la base de datos
     $sql = "INSERT INTO alumnos (nombre, apellido_paterno, apellido_materno, fecha_nacimiento, edad) VALUES ('$nombre', '$apellidoPaterno', '$apellidoMaterno', '$fechaNacimiento', $edad)";
