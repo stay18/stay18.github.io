687459ef73c64a274b2186e4fecdd565db1aae6c
stay18 <146612234+stay18@users.noreply.github.com>
2023-10-04 21:42:56 -0600

    Update Datos.php
    av

--- Datos.php
+++ Datos.php
@@ -19,7 +19,6 @@ if ($conn->connect_error) {
 }
 
 
-$sql = "INSERT INTO tabla  VALUES ('$nombre', '$pass')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Registro insertado correctamente";
@@ -35,5 +34,40 @@ if ($conn->query($sql) === TRUE) {
 // Cerrar la conexión cuando hayas terminado
 $conn->close();
 
+
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $apellidoPaterno = $_POST["apellido_paterno"];
+    $apellidoMaterno = $_POST["apellido_materno"];
+    $fechaNacimiento = $_POST["fecha_nacimiento"];
+    
+    // Calcular la edad a partir de la fecha de nacimiento
+    $fechaNacimiento = new DateTime($fechaNacimiento);
+    $hoy = new DateTime();
+    $edad = $hoy->diff($fechaNacimiento)->y;
+    
+    // Puedes imprimir la edad para verificar
+    // echo "Edad: " . $edad;
+    
+    // Aquí puedes realizar operaciones con los datos, como guardarlos en una base de datos
+    // Por ejemplo, puedes usar una conexión a una base de datos MySQL para guardar los datos
+    // Reemplaza los siguientes valores con los datos de tu base de datos
+  
+    
+    // Insertar los datos en la base de datos
+    $sql = "INSERT INTO alumnos (nombre, apellido_paterno, apellido_materno, fecha_nacimiento, edad) VALUES ('$nombre', '$apellidoPaterno', '$apellidoMaterno', '$fechaNacimiento', $edad)";
+    
+    if ($conexion->query($sql) === TRUE) {
+        echo "Registro exitoso";
+    } else {
+        echo "Error al registrar el alumno: " . $conexion->error;
+    }
+    
+    // Cerrar la conexión a la base de datos
+    $conexion->close();
+}
+?>
 
 
